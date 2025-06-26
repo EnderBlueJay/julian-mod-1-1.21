78b0b8ddf6d8e80f3607f20d328ca222b039fa43
EnderBlueJay <enderbeardragon@gmail.com>
2025-06-26 15:59:15 -0700

    comitted

---
+++ src/main/java/net/julian/modid/JulianMod1Client.java
@@ -0,0 +1,10 @@
+package net.julian.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JulianMod1Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
